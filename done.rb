f7bd152388dc3215533dc8c3c40f8042f36af899
mariselagonzalez <37843508+mariselagonzalez@users.noreply.github.com>
2020-01-24 15:45:23 -0800

    Add files via upload

---
+++ done.rb
@@ -0,0 +1,33 @@
+
+use_bpm 145
+live_loop:backie do
+  1.times do
+    play :a4
+    sleep 0.6
+    play :a4
+    sleep 0.6
+    play :f4
+    sleep 0.6
+    play :b4
+    sleep 0.6
+    play :a4
+    sleep 0.6
+    play :f4
+    sleep 0.8
+    play :f4
+    sleep 0.6
+    play :a4
+    sleep 0.6
+    play :a4
+    sleep 0.6
+    play :f4
+    sleep 0.6
+    play :b4
+    sleep 0.6
+    play :a4
+    sleep 0.6
+    play :f4
+    sleep 0.6
+    sample :bd_ada
+  end
+end
